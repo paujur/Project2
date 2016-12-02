cd5dd1f02d8b8265c26ff9222f7042969b714f79
Paul Jurczyk <pauljurczykny@gmail.com>
2016-12-02 12:04:48 -0500

    now i created project 2

---
+++ FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedrReadr-PaulJurczyk
+//
+//  Created by Paul Jurczyk on 12/2/16.
+//  Copyright © 2016 Paul Jurczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
