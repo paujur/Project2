e7dbfac9b9d5328d76e4310d993bc22dc786e05b
Paul Jurczyk <pauljurczykny@gmail.com>
2017-01-02 19:03:37 -0500

    almost works entirely. need to fix reloading data and array

--- FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MainViewController.swift
+++ FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MainViewController.swift
@@ -23,9 +23,9 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     var pagination = "&paginationInput.entriesPerPage=5"
     var responseEncoding = "&X-EBAY-API-RESPONSE-ENCODING=JSON"
     var restPayload = "&REST-PAYLOAD"
-    var callback = "fetchData"
+    var callback = "fetchData" // I believe this needs to be the same as teh function that's calling the API call
     var dataFormat = "&RESPONSE-DATA-FORMAT=JSON"
-    var fetchedResult = [String : Any]()
+    //var fetchedResult = [String : Any]()
     
     
     // MARK: IBOutlets ---------------------------------------------
@@ -36,94 +36,104 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // MARK: IBActions ------------------------------
     
     @IBAction func searchButtonTapped(_ sender: UIButton) {
-        
+        //fetchedItems = [EbayItem]()
+        //self.newEndPoint = "http://svcs/ebay.com/services/search/FindingService/v1?"
         searchQuery = searchTextField.text!
         let keyword = "&keywords=" + searchQuery
         let encodedURL = newEndPoint + operationName + version + appName + globalId + keyword + pagination + dataFormat + restPayload + callback
         newEndPoint = encodedURL
         //print(newEndPoint)
         fetchData() { result in
-             self.newEndPoint = "http://svcs/ebay.com/services/search/FindingService/v1?"
+            
             do {
-             
+                
                 
                 let jsonObject = try JSONSerialization.jsonObject(with: result, options: .mutableContainers) as! [String : Any]
-                let instance = EbayItem(jsonObject: jsonObject)
-               //print(instance.name)
                 
-              // let resultsTopLayer = jsonObject["findItemsByKeywordsResponse"] as? [String : Any]
-//                let results = resultsTopLayer?["searchResult"] as? [String : Any]
-//                let item = results?["item"] as? [String : Any]
-//                let titleArray = item?["title"] as? [String]
-//                let title = titleArray?[0]
-//                let categoryArray = item?["categoryName"] as? [String]
-//                let category = categoryArray?[0]
-//                let galleryURLArray = item?["galleryURL"] as? [String]
-//                let galleryURL = galleryURLArray?[0]
-//                let itemURLArray = item?["viewItemURL"] as? [String]
-//                let itemURL = itemURLArray?[0]
-//                let priceInfo = item?["currentPrice"] as? [String : Any]
-//                let price = priceInfo?["__value__"] as? String
-//                let conditionInfo = item?["condition"] as? [String : Any]
-//                let condition = conditionInfo?["conditionDisplayName"] as? String
+                
+                
+                let resultsTopLayer = jsonObject["findItemsByKeywordsResponse"] as? [Any]
+                let oneMoreLayer = resultsTopLayer?[0] as? [String : Any]
+                let results = oneMoreLayer?["searchResult"] as? [Any]
+                let actualResults = results?[0] as? [String : Any]
+                let items = actualResults?["item"] as? [Any]
+                for item in items! {
+                    let newItem = EbayItem(jsonObject: item as! [String : Any])
+                    fetchedItems.append(newItem)
+                }
+                
+                //let itemTitle =
+                //                let titleArray = item?["title"] as? [String]
+                //                let title = titleArray?[0]
+                //                let categoryArray = item?["categoryName"] as? [String]
+                //                let category = categoryArray?[0]
+                //                let galleryURLArray = item?["galleryURL"] as? [String]
+                //                let galleryURL = galleryURLArray?[0]
+                //                let itemURLArray = item?["viewItemURL"] as? [String]
+                //                let itemURL = itemURLArray?[0]
+                //                let priceInfo = item?["currentPrice"] as? [String : Any]
+                //                let price = priceInfo?["__value__"] as? String
+                //                let conditionInfo = item?["condition"] as? [String : Any]
+                //                let condition = conditionInfo?["conditionDisplayName"] as? String
             }
             catch {
                 print("we gots an error")
             }
         }
-       
+        
     }
     
     
     func fetchData(closure: @escaping (Data) -> ()) {
-     
-            var endpoint = newEndPoint
-            var url = URLRequest(url: URL(string: endpoint)!)
+        
+        let endpoint = newEndPoint
+        let url = URLRequest(url: URL(string: endpoint)!)
         // need to make this a POST in order to receive JSON, not GET
-            //url.httpMethod = "GET"
+        //url.httpMethod = "GET"
         // this tries to add the right parameter to the header
-            //url.setValue("JSON", forHTTPHeaderField: "X-EBAY-SOA-RESPONSE-DATA-FORMAT")
-            let session = URLSession(configuration: URLSessionConfiguration.default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                // TODO: add error handling
-               
-                guard let responseData = data else {
-                   // self.itemTextView.text = "Error: did not receive data"
-                    return
-                }
-                DispatchQueue.main.async {
-                    closure(responseData)
-                }
+        //url.setValue("JSON", forHTTPHeaderField: "X-EBAY-SOA-RESPONSE-DATA-FORMAT")
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            // TODO: add error handling
+            
+            guard let responseData = data else {
+                // self.itemTextView.text = "Error: did not receive data"
+                return
             }
+            DispatchQueue.main.async {
+                closure(responseData)
+                self.mainTableView.reloadData()                }
+        }
         
         
-            task.resume()
+        task.resume()
         
     }
     
     // MARK: Tableview Protocols
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResult.count
+        return fetchedItems.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! MyTableViewCell
-       // cell.itemTitleLabel.text = fetchedResult[indexPath.row]
+        cell.itemTitleLabel.text = fetchedItems[indexPath.row].title
         
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
--- FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/eBay.swift
+++ FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/eBay.swift
@@ -13,25 +13,18 @@ import Foundation
 var fetchedItems = [EbayItem]()
 
 class EbayItem {
-    var name: String
+    var title: String
     var price: String
     //var description: String
     init(jsonObject: [String: Any]){
-        let resultsTopLayer = jsonObject["findItemsByKeywordsResponse"] as? [String : Any]
-        let results = resultsTopLayer?["searchResult"] as? [String : Any]
-        let item = results?["item"] as? [String : Any]
-        let titleArray = item?["title"] as? [String]
-        name = (titleArray?[0])!
-        let categoryArray = item?["categoryName"] as? [String]
-        let category = categoryArray?[0]
-        let galleryURLArray = item?["galleryURL"] as? [String]
-        let galleryURL = galleryURLArray?[0]
-        let itemURLArray = item?["viewItemURL"] as? [String]
-        let itemURL = itemURLArray?[0]
-        let priceInfo = item?["currentPrice"] as? [String : Any]
-        price = (priceInfo?["__value__"] as? String)!
-        let conditionInfo = item?["condition"] as? [String : Any]
-        let condition = conditionInfo?["conditionDisplayName"] as? String
+        print(jsonObject)
+        let titleArray = jsonObject["title"] as? [String]
+        title = (titleArray?[0])!
+        let sellingStatus = jsonObject["sellingStatus"] as? [Any]
+        let currentSellingStatus = sellingStatus?[0] as? [String : Any]
+        let currentPrices = currentSellingStatus?["currentPrice"] as? [Any]
+        let currentPrice = currentPrices?[0] as? [String : Any]
+        price = currentPrice?["__value__"] as! String
     }
    
     
@@ -41,3 +34,26 @@ class EbayItem {
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
