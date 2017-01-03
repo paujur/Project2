8b320676d8376204fbee78f2d37e4c3d7c0a2671
Paul Jurczyk <pauljurczykny@gmail.com>
2017-01-03 10:49:32 -0500

    no good

--- FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MyTableViewCell.swift
+++ FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MyTableViewCell.swift
@@ -14,10 +14,14 @@ class MyTableViewCell: UITableViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     
     @IBOutlet weak var itemPriceLabel: UILabel!
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        UIView.animate(withDuration: 12.0, delay: 1, options: ([.curveLinear, .repeat]), animations: {() -> Void in
+            self.itemTitleLabel.center = CGPoint(x: 0 - self.itemTitleLabel.bounds.size.width / 2, y: self.itemTitleLabel.center.y)
+        }, completion:  { _ in })
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
