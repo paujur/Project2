4f2ce08532f6c2a3d091d8a73c822f82d3281da5
Paul Jurczyk <pauljurczykny@gmail.com>
2017-01-03 10:51:30 -0500

    that’s it for project 2 i think

--- FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MyTableViewCell.swift
+++ FeedrReadr-PaulJurczyk/FeedrReadr-PaulJurczyk/MyTableViewCell.swift
@@ -19,9 +19,7 @@ class MyTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        UIView.animate(withDuration: 12.0, delay: 1, options: ([.curveLinear, .repeat]), animations: {() -> Void in
-            self.itemTitleLabel.center = CGPoint(x: 0 - self.itemTitleLabel.bounds.size.width / 2, y: self.itemTitleLabel.center.y)
-        }, completion:  { _ in })
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
